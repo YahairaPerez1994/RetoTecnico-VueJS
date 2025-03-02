7f1eecca5366e169e167fb4de14ecc46b06c459f
YahairaPerez1994 <yahairaperez2694@gmail.com>
2025-03-01 19:19:36 -0500

    Se configuró vueJs con Vue Router y se creo los componentes inciales: Postlist para mostrar la lisa de posts y PostDetail para ver detalle de un post

---
+++ yahaira/vue-posts-app/src/components/PostDetail.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>Detalle del Post</h1>
+      <p>Aquí se mostrará la información del post seleccionado.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "PostDetail",
+  };
+  </script>
+  
