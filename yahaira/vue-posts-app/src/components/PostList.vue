0b4eb6db750846131bf23ff7379c7a840b0ef6f3
YahairaPerez1994 <yahairaperez2694@gmail.com>
2025-03-01 20:08:29 -0500

    Carga dinamicamente de posts en: PostList.vue desde la Api, aqui ya se muestra el title y el body de cada posts

--- yahaira/vue-posts-app/src/components/PostList.vue
+++ yahaira/vue-posts-app/src/components/PostList.vue
@@ -1,13 +1,30 @@
 <template>
-    <div>
-      <h1>Listado de Posts</h1>
-      <p>Aquí se mostrarán los posts.</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "PostList",
-  };
-  </script>
-  
+  <div>
+    <h1>Listado de Posts</h1>
+    <p>Aquí se mostraran los posts.</p>
+    
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <h3>{{ post.title }}</h3>
+        <p>{{ post.body }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+
+export default {
+  setup() {
+    const posts = ref([]);
+
+    onMounted(async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+      posts.value = await response.json();
+    });
+
+    return { posts };
+  },
+};
+</script>
